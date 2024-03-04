5cb9aaaad6246bfb53d1b4e7070a029c28dae2fb
mustaqeemjahangir <mustaqeemspackturm2005@gmail.com>
2024-03-04 06:17:51 +0500

    create a index.html and app.js

---
+++ app.js
@@ -0,0 +1,2 @@
+alert ( 'Welcome To JS World!')
+document.write("<h1>Hello Javasript Plannet </h1>  <p>I am the master of my <b>own</b> fate </p> <p>I am the <b>champion</b> of my own soule </p> <p>I am the <b>superhero</b> of my own life</p>")
